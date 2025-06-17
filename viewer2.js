db905487cb439159dc370edc7e394323a18c5a2d
Muravyev <31532085+Muravyev@users.noreply.github.com>
2025-06-18 00:01:03 +0300

    Add files via upload

--- viewer2.js
+++ viewer2.js
@@ -1,1 +1,24 @@
-// no script in base version
+function loadPage(filename) {
+  fetch(filename)
+    .then(response => response.text())
+    .then(data => {
+      document.getElementById("content").innerHTML = data;
+      document.getElementById("content").style.display = "block";
+      document.getElementById("main-buttons").style.display = "none";
+      document.getElementById("nav").style.display = "block";
+    });
+}
+function goBack() {
+  document.getElementById("content").style.display = "none";
+  document.getElementById("main-buttons").style.display = "block";
+  document.getElementById("nav").style.display = "none";
+}
+function goHome() {
+  goBack();
+}
+window.addEventListener("DOMContentLoaded", function() {
+  if (Telegram.WebApp?.initDataUnsafe?.user?.first_name) {
+    const user = Telegram.WebApp.initDataUnsafe.user;
+    document.getElementById("welcome-text").innerText = `${user.first_name}, добро пожаловать в мини-приложение приёмной комиссии СГМУ!`;
+  }
+});
